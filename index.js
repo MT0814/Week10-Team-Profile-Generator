c9a2ad92cc3794d9d2d0287d72b5332501db5fca
millie_tsai <millie_tsai@me.com>
2021-10-27 15:47:56 -0700

    updated css and email code in js

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ function addHtml(member) {
                     </h5>
                     <ul class="list-group list-group-flush">
                        <li class="list-group-item"><i class="bi bi-person"></i>  ${id}</li>
-                       <li class="list-group-item"><i class="bi-mailbox"></i><a href="mailto: ${email} target="_blank">${email}</a></li>
+                       <li class="list-group-item"><i class="bi-mailbox"></i><a href="mailto: ${email}" target="_blank">  ${email}</a></li>
                        <li class="list-group-item"><i class="bi-github" role="img" aria-label="GitHub"></i><a href="https://github.com/${gitHub}" target="_blank">  ${gitHub}</a></li>
                     </ul>
                 </div>
@@ -210,7 +210,7 @@ function addHtml(member) {
                 <h5 class="card-header">${name}<br><i class="bi bi-person-badge"> Intern</i></h5>
                 <ul class="list-group list-group-flush">
                 <li class="list-group-item"><i class="bi bi-person"></i>  ${id}</li>
-                <li class="list-group-item"><i class="bi-mailbox"></i><a href="mailto: ${email} target="_blank">${email}</a></li>
+                <li class="list-group-item"><i class="bi-mailbox"></i><a href="mailto: ${email}" target="_blank">  ${email}</a></li>
                     <li class="list-group-item"><i class="bi bi-book"></i>  ${school}</li>
                 </ul>
             </div>
@@ -222,7 +222,7 @@ function addHtml(member) {
                 <h5 class="card-header">${name}<br /><i class="bi bi-bookmark-star-fill"> Manager</i></h5>
                   <ul class="list-group list-group-flush">
                   <li class="list-group-item"><i class="bi bi-person"></i>  ${id}</li>
-                  <li class="list-group-item"><i class="bi-mailbox"></i><a href="mailto: ${email} target="_blank">${email}</a></li>
+                  <li class="list-group-item"><i class="bi-mailbox"></i><a href="mailto: ${email}" target="_blank">  ${email}</a></li>
                     <li class="list-group-item"><i class="bi bi-telephone"></i>  ${officePhone}</li>
                   </ul>
             </div>
